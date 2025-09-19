95361ef13c604565cad6a8edafd2efa6dd45b365
Ankush Khairnar <ankushkhairnar12@gmail.com>
2025-09-19 14:57:05 +0530

    image config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -7,6 +7,10 @@ const nextConfig = {
         hostname: "lh3.googleusercontent.com",
       },
       {
+        protocol: "https",
+        hostname: "sweet-shop-incubyte.vercel.app"
+      },
+      {
         protocol: "http", 
         hostname: "localhost",
         port: "3001",
